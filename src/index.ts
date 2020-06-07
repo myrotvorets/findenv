630d2ebcc541629075654efc2f55f996186e02c5
Myrotvorets <support@myrotvorets.center>
2020-06-07 23:14:46 +0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import { dirname, join, resolve } from 'path';
+import { promisify } from 'util';
+
+const exists = promisify(fs.exists);
+export async function findEnv(): Promise<string | null> {
+    const locations: string[] = [];
+    if (require.main && require.main.filename) {
+        locations.push(resolve(join(dirname(require.main.filename), '.env')));
+        locations.push(resolve(join(dirname(require.main.filename), '..', '.env')));
+    }
+
+    const cwd = process.cwd();
+    locations.push(resolve(join(cwd, '.env')));
+
+    for (const location of locations) {
+        if (await exists(location)) {
+            return location;
+        }
+    }
+
+    return null;
+}
